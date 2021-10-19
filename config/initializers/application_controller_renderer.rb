abed2d3666079f9f3670adb364ad68d88ca3012a
susuki22 <shoya.s.0427@gmail.com>
2021-10-19 11:06:45 +0000

    [Update]5-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b35b38f6a3affe6b7edb8f783219a09f8a107860d11eb868fb08aaeabcbf54af65af5b649caa728fb7712fcc7f7a2d75d86f2a9f86fc04b2a69f85297cf57635'
